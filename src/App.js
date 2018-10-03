2fa638d82cb550332f0a0aa9ed4ab444e9c496d5
unknown <caylenepicache@gmail.com>
2018-10-02 20:51:54 -0700

    updating app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 //imports dependencies and files
 import React, { Component } from "react";
 import axios from 'axios';
+import BarChart from "./components/BarChart/BarChart.js";
 
-export default class NameList extends React.Component {
+export default class App extends React.Component {
   
   constructor(props){
     super(props);
-    state = {
-    names:[]
+
+    this.state = {
+    dataNames:[],
+    topTen: []
     };
   }
 
@@ -15,25 +18,46 @@ export default class NameList extends React.Component {
     //promised based
     axios.get(`https://data.cityofnewyork.us/api/views/25th-nujf/rows.json`)
       .then(res => {
-        this.setState({names: res.data});
 
         console.log(res);
         //console.log(res.data.data)
         console.log(res.data.data)
-        console.log(res.data.data["13"][11]);
+        console.log(res.data.data[13][11]);
+
+        //get values and place in arrays 
+        let topTenArr = [];
+        let yearArr = [];
+        let ethnicityArr = [];
 
         for(var i=0; i < res.data.data.length; i++) {
           //console.log(i);
           //top 10 names on the every list possible
-          if(res.data.data[i][13] <= 10) {
-           // console.log(res.data.data[i]["8"]);
+          if(res.data.data[i]["13"] <= 10) {
+            //console.log(res.data.data[i][12]);
+            topTenArr.push(res.data.data[i][12])
+           //console.log(res.data.data[i]["8"]);
             //console.log(res.data.data[i]["9"]);
             //console.log(res.data.data[i]["10"]);
             //console.log(res.data.data[i]["11"]);
-            //console.log(res.data.data[i]["12"]);
+            //console.log(res.data.data[i][12]);
             //console.log(res.data.data[i]["13"]);
           }
+          
+          if(!yearArr.includes(res.data.data[i][8])) {
+            //console.log(res.data.data[i][8]);
+            yearArr.push(res.data.data[i][8]);
+          }
+
+          if(!ethnicityArr.includes(res.data.data[i][10])) {
+            //console.log(res.data.data[i][10]);
+            yearArr.push(res.data.data[i][10]);
+          }
         }
+
+        //check arrays
+        console.log("topTenArr" + topTenArr);
+        console.log("YearArr" + yearArr);
+        console.log("ethnicityArr" + ethnicityArr);
       })
       .catch(err => {
         console.log(err);
@@ -41,12 +65,12 @@ export default class NameList extends React.Component {
   }
 
   render() {
-    console.log("in render" + this.state.names.map);
+    //console.log("in render" + this.state.names.map);
+    //const child = this.state.names.map((element,index) => {
     return (
       <div>
-        <ul>{this.state.names.map(function(object,i){return <li obj={object} key = {i} />})};
-        </ul>
+        <BarChart />
       </div>
-    )
-  }
-}
+    //}
+    );
+  }}
